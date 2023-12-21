1fc7472a845130ec85511076eb892587e66ca028
Golden Dennar <gdennar@yahoo.co.uk>
2023-12-21 21:57:45 +0000

    Change headers UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import SpinnerImg from "../src/assests/spinner.jpg";
 
+const PreHeader = lazy(() => import("./components/PreHeader"));
 const Header = lazy(() => import("./components/Header"));
+const Categories = lazy(() => import("./components/categories/Categories"));
 const Home = lazy(() => import("./pages/home/Home"));
 const Contact = lazy(() => import("./pages/contact/Contact"));
 const Login = lazy(() => import("./pages/auth/Login"));
@@ -11,7 +13,7 @@ const Reset = lazy(() => import("./pages/auth/Reset"));
 const AdminRoute = lazy(() => import("./components/adminRoute/AdminRoute"));
 const Admin = lazy(() => import("./pages/admin/Admin"));
 const ProductDetails = lazy(() =>
-	import("./components/product/ProductDetails")
+  import("./components/product/ProductDetails")
 );
 const Cart = lazy(() => import("./pages/cart/Cart"));
 const CheckoutDetails = lazy(() => import("./pages/checkout/CheckoutDetails"));
@@ -20,57 +22,59 @@ const CheckoutSuccess = lazy(() => import("./pages/checkout/CheckoutSuccess"));
 const OrderHistory = lazy(() => import("./pages/orderHistory/OrderHistory"));
 const OrderDetails = lazy(() => import("./pages/orderDetails/OrderDetails"));
 const ReviewProducts = lazy(() =>
-	import("./components/reviewProducts/ReviewProducts")
+  import("./components/reviewProducts/ReviewProducts")
 );
 const NotFound = lazy(() => import("./pages/notFound/NotFound"));
 const Footer = lazy(() => import("./components/Footer"));
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Suspense
-					fallback={
-						<div className="lazy-loading">
-							<img
-								src={SpinnerImg}
-								alt="Loading..."
-								style={{ width: "50px" }}
-							/>
-						</div>
-					}
-				>
-					<Header />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/contact" element={<Contact />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/register" element={<Register />} />
-						<Route path="/reset-password" element={<Reset />} />
+  return (
+    <>
+      <BrowserRouter>
+        <Suspense
+          fallback={
+            <div className="lazy-loading">
+              <img
+                src={SpinnerImg}
+                alt="Loading..."
+                style={{ width: "50px" }}
+              />
+            </div>
+          }
+        >
+          <PreHeader />
+          <Header />
+          <Categories />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/reset-password" element={<Reset />} />
 
-						<Route
-							path="/admin/*"
-							element={
-								<AdminRoute>
-									<Admin />
-								</AdminRoute>
-							}
-						/>
-						<Route path="/product-details/:id" element={<ProductDetails />} />
-						<Route path="/cart" element={<Cart />} />
-						<Route path="/checkout-details" element={<CheckoutDetails />} />
-						<Route path="/checkout" element={<Checkout />} />
-						<Route path="/checkout-success" element={<CheckoutSuccess />} />
-						<Route path="/order-history" element={<OrderHistory />} />
-						<Route path="/order-details/:id" element={<OrderDetails />} />
-						<Route path="/review-product/:id" element={<ReviewProducts />} />
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-					<Footer />
-				</Suspense>
-			</BrowserRouter>
-		</>
-	);
+            <Route
+              path="/admin/*"
+              element={
+                <AdminRoute>
+                  <Admin />
+                </AdminRoute>
+              }
+            />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout-details" element={<CheckoutDetails />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/checkout-success" element={<CheckoutSuccess />} />
+            <Route path="/order-history" element={<OrderHistory />} />
+            <Route path="/order-details/:id" element={<OrderDetails />} />
+            <Route path="/review-product/:id" element={<ReviewProducts />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Suspense>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,59 @@
 import SearchIcon from "@mui/icons-material/Search";
-import React from "react";
-import "./Search.css";
+import React, { useEffect, useState } from "react";
+import classes from "./Search.module.css";
+import { filterAction } from "../store/filterSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Search = (props) => {
-	return (
-		<div className="search">
-			<form>
-				<SearchIcon className="icon" />
-				<input
-					placeholder="Search by name"
-					value={props.search}
-					onChange={props.onChange}
-					type="text"
-					className={`search-input ${props.className}`}
-				/>
-			</form>
-		</div>
-	);
+  const [category, setCategory] = useState("All");
+  const products = useSelector((state) => state.product.products);
+
+  const dispatch = useDispatch();
+
+  const handleSelectChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const allCategory = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  useEffect(() => {
+    dispatch(filterAction.filterByCategory({ products, category }));
+  }, [dispatch, products, category]);
+
+  return (
+    <section className={classes.search}>
+      <div className={classes.searchForm}>
+        <SearchIcon className={classes.icon} />
+        <input
+          placeholder="Search by name"
+          value={props.search}
+          onChange={props.onChange}
+          type="text"
+          className={`${classes.searchInput}`}
+        />
+
+        <select
+          onChange={handleSelectChange}
+          className={classes.categoryDropdown}
+        >
+          {allCategory.map((cat) => {
+            return (
+              <option
+                key={cat}
+                className={`${category === cat ? `active` : null}`}
+                value={cat}
+              >
+                {cat}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </section>
+  );
 };
 
 export default Search;
